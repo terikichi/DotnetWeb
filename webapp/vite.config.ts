0dd0ef73b7279c47a868ca2183ee662af605cdae
Ryo Terui <r.terui89@gmail.com>
2025-02-21 23:38:09 +0000

    本番環境用ファイルの生成と動作用設定を追加2

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173,
     proxy: {
-      '/api': {
+      '^/api': {
         target: 'http://webapi:5000',
         changeOrigin: true,
         secure: false
